5525833870bb972453d2e6b9caae0640b6313eca
Krishna Sundarram <krishna.sundarram@gmail.com>
2012-06-13 00:35:28 +0530

    started voiceservice

---
+++ src/com/sundarram/VoiceService.java
@@ -0,0 +1,12 @@
+package com.sundarram;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+
+public class VoiceService extends Service {
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
